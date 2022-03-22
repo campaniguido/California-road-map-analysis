97369addeda6045b3fbb0a75a53475721d591026
Guido Campani <guido.campani@studio.unibo.it>
2022-03-23 00:17:05 +0100

    parameters, save in directory

--- parameters.py
+++ parameters.py
@@ -4,10 +4,15 @@ Created on Mon Mar 21 22:00:48 2022
 
 @author: Guido
 """
-
+import os
 
 file_position='https://raw.githubusercontent.com/campaniguido/Software_and_Computing/main/roadnet-ca.txt'
 number_of_edge=5000
+path_to_save_data=os.getcwd()
+
+
+
+
 save_fig=True
 extention='png'
 n_step_degree=50
