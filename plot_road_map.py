3f0ad070606abd5e9b562a5f330b803de714dcd1
Guido Campani <guido.campani@studio.unibo.it>
2022-03-24 19:44:50 +0100

    add plot of ERG

--- plot_road_map.py
+++ plot_road_map.py
@@ -59,3 +59,30 @@ fn.Feature_cumulative_evolution(Clustering_evolution[:,4:], 'Clustering',params.
 CC_evolution=np.array(pd.read_csv(params.path_to_save_data+'\CC_evolution.csv'))   
 fn.Feature_mean_evolution(CC_evolution[:,1],CC_evolution[:,2:4], 'Closeness',params.save_fig, params.extention)
 fn.Feature_cumulative_evolution(CC_evolution[:,4:], 'Closeness centrality',params.save_fig, params.extention)
+
+
+
+#%%     PLOT ERG
+
+#%% Histograms
+data_ERG=pd.read_csv(params.path_to_save_data+'\data_ERG.csv')
+
+fn.Hist_plot(data_ERG.Degree,color[1], 'Degree_ERG distribution', params.save_fig, params.extention)
+fn.Hist_plot(data_ERG.Betweeness_Centrality,color[1], 'Betweeness_Centrality_ERG distribution', params.save_fig, params.extention)
+fn.Hist_plot(data_ERG.Closeness_Centrality,color[1], 'Closeness_Centrality_ERG distribution', params.save_fig, params.extention)
+fn.Hist_plot(data_ERG.Clustering,color[1], 'Clustering_ERG distribution', params.save_fig, params.extention)
+
+#%% Scatterplot
+
+
+fn.Scatter_plot(data_ERG.community, 'Community' , data_ERG.Degree, 'Degree', data_ERG.community,params.save_fig, params.extention)
+fn.Scatter_plot(data_ERG.community, 'Community' , data_ERG.Clustering, 'Clustering', data_ERG.community,params.save_fig, params.extention)
+fn.Scatter_plot(data_ERG.community, 'Community' , data_ERG.Closeness_Centrality, 'Closeness_Centrality', data_ERG.community,params.save_fig, params.extention)
+fn.Scatter_plot(data_ERG.community, 'Community' , data_ERG.Betweeness_Centrality, 'Betweeness_Centrality', data_ERG.community,params.save_fig, params.extention)
+
+#_______________features vs degree______
+fn.Scatter_plot(data_ERG.Betweeness_Centrality, 'Betweeness_Centrality' , data_ERG.Degree, 'Degree', data_ERG.community,params.save_fig, params.extention)
+fn.Scatter_plot(data_ERG.Closeness_Centrality, 'Closeness_Centrality' , data_ERG.Degree, 'Degree', data_ERG.community,params.save_fig, params.extention)
+fn.Scatter_plot(data_ERG.Betweeness_Centrality, 'Betweeness_Centrality' , data_ERG.Degree, 'Degree', data_ERG.community,params.save_fig, params.extention)
+
+
