b26babef8ea32cb0bb940576151572e1c907dad6
Guido Campani <guido.campani@studio.unibo.it>
2022-03-25 00:26:20 +0100

    parameters file added

---
+++ parameters_Copycat.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 24 19:54:33 2022
+
+@author: Guido
+"""
+
+import os
+
+file_position='https://raw.githubusercontent.com/campaniguido/Software_and_Computing/main/roadnet-ca.txt'
+number_of_edge=5000
+path_to_save_data=os.getcwd()
+save_fig=True
+extention='png'
+nstep=50
