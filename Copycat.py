bc200a570d1e419be7f9c862440864c7a89d1625
Guido Campani <guido.campani@studio.unibo.it>
2022-04-04 19:34:52 +0200

    Correction due to the modification of the function conditional prob

--- Copycat.py
+++ Copycat.py
@@ -36,10 +36,10 @@ step=max(dct_dist_link.values())/paramsC.nstep
 distance_frequency=fn.Node_distance_frequency(dct_dist,paramsC.nstep,step)       
 distance_link_frequency=fn.Node_distance_frequency(dct_dist_link,paramsC.nstep,step) 
 
-distance_linking_probability=fn.Conditional_probability(dct_dist_link,paramsC.nstep,distance_frequency)
+distance_linking_probability=fn.Conditional_probability(distance_link_frequency,paramsC.nstep,distance_frequency)
 Copy_map=fn.SuperGraph()
 Copy_map.add_nodes_from(list(G.nodes))
-Copy_map=fn.Add_edges_from_map(Copy_map, map_dct, distance_linking_probability)
+fn.Add_edges_from_map(Copy_map, dct_dist, distance_linking_probability)
 
             
 
