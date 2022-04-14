e8a646c69811b989f435f3ff0cfb5fd06e3ba9a3
Guido Campani <guido.campani@studio.unibo.it>
2022-04-14 14:30:56 +0200

    comments on the paramenters

--- parameters.py
+++ parameters.py
@@ -6,20 +6,28 @@ Created on Mon Mar 21 22:00:48 2022
 """
 import os
 
-
-
-file_position='https://raw.githubusercontent.com/campaniguido/Software_and_Computing/main/roadnet-ca.txt'
-number_of_edge=5020
-name_simulation='road_'+str(number_of_edge)
-path_to_save_data=os.getcwd()+'\\'+name_simulation
 seed=3
 
+#path where the data are stored 
+file_position='https://raw.githubusercontent.com/campaniguido/Software_and_Computing/main/roadnet-ca.txt'
+#numer of edges to take into account
+number_of_edge=5020
+#name of the simulation file
+name_simulation='road_'+str(number_of_edge)
+#path where the data will be stored
+path_to_save_data=os.getcwd()+'\\'+name_simulation
 
-file_to_plot=os.getcwd()+'\\'+name_simulation
-save_fig=False
-extention='png'
+#number of steps in the size evoution simultation
 n_step_degree=50
 n_step=10
+
+
+#path where the simulation data to plot are stored and where it will save the plot
+file_to_plot=os.getcwd()+'\\'+name_simulation
+#bool-> save_fig=True it saves the plot in the directory file_to_plot ||| save_fig=False it just shows the plot
+save_fig=False
+extention='png'
+
 
 
 
--- parameters_Copycat.py
+++ parameters_Copycat.py
@@ -7,15 +7,25 @@ Created on Thu Mar 24 19:54:33 2022
 
 import os
 
+seed=3
 
+#path where the data are stored 
 file_position='https://raw.githubusercontent.com/campaniguido/Software_and_Computing/main/roadnet-ca.txt'
+#numer of edges of the simulated network
 number_of_edge=5000
+#name of the simulation file
 name_simulation='Copycat'+str(number_of_edge)
+#path where the data will be stored
 path_to_save_data=os.getcwd()+'\\'+name_simulation
 
+
+#path where the simulation data to plot are stored and where it will save the plot
 file_to_plot=os.getcwd()+'\\'+name_simulation
+
+#bool-> save_fig=True it saves the plot in the directory file_to_plot ||| save_fig=False it just shows the plot
 save_fig=True
 extention='png'
+#number of bins of the distance distribution
 nstep=50
 
-seed=3
+
