efc54e549ef2449a6c2b8e187a853c3abb3ec238
Guido Campani <guido.campani@studio.unibo.it>
2022-04-20 13:56:06 +0200

    figure True

--- parameters.py
+++ parameters.py
@@ -11,7 +11,7 @@ seed=3
 #path where the data are stored 
 file_position='https://raw.githubusercontent.com/campaniguido/Software_and_Computing/main/roadnet-ca.txt'
 #numer of edges to take into account
-number_of_edge=5020
+number_of_edge=5021
 #name of the simulation file
 name_simulation='road_'+str(number_of_edge)
 #path where the data will be stored
@@ -25,7 +25,7 @@ n_step=10
 #path where the simulation data to plot are stored and where it will save the plot
 file_to_plot=os.getcwd()+'\\'+name_simulation
 #bool-> save_fig=True it saves the plot in the directory file_to_plot ||| save_fig=False it just shows the plot
-save_fig=False
+save_fig=True
 extention='png'
 
 
